2acaee03f84e02077168e0c4f4ba6fb974573c4e
Pat-devs <95124571+Pat-devs@users.noreply.github.com>
2026-08-10 10:29:52 +0200

    move person class to its own file

--- Program.cs
+++ Program.cs
@@ -19,19 +19,3 @@ class Program
 }
 
 
-class Person
-{
-    // by default methods are inaccessible outside of the class (use for example public to make them accessible from outside)
-    public static void Greeter()
-    {
-        Console.WriteLine("Hi, i am a person");
-
-        TestMethod();
-    }
-
-    // this method is inaccessible from outside, but accessible from within Person-class or its other methods.
-    static void TestMethod()
-    {
-        Console.WriteLine("Internal test method running :)");
-    }
-}
