a8ad47707d66e488ef726434dc66e10c144fd3a4
Pat-devs <95124571+Pat-devs@users.noreply.github.com>
2026-08-10 09:43:02 +0200

    overloads in built in writeline method

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace BackendUke3;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // example overloads
+        Console.WriteLine(1);
+        Console.WriteLine(0.5);
+        Console.WriteLine("text");
+    }
+}
